6db909d39cd063f378f075ee13026f66ea2da384
UpdateDeveloper <solarmoff@gmail.com>
2025-12-18 00:09:47 +0300

    feat(parser): Добавлен парсер .toml конфига

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 // SPDX-License-Identifier: EPL-2.0
 
 mod build_system;
+mod parser;
 
 use build_system::{BuildSystem, Actions};
 
@@ -14,7 +15,13 @@ fn main() {
 
     match action {
         Actions::build_apk => {
+            let config = parser::load(get_paths(build_system.args))
+                .expect("Failed to create config from toml files");
 
+            build_system.set_config(config);
+            
+            // UNSAFE TESTING
+            println!("Package.package: {}", config.package.unwrap().package.unwrap());
         },
 
         Actions::help => {
@@ -23,4 +30,14 @@ fn main() {
 
         _ => todo!(),
     }
+}
+
+/// [WAIT DOC]
+fn get_paths(args: Vec<String>) -> Vec<String> {
+    let len = args.len();
+
+    if len == 2 { return args[2..2].to_vec(); }
+    else if len > 2 { return args[2..len].to_vec(); }
+
+    return vec![]
 }
